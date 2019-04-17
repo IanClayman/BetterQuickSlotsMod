381017bd8922ed16532cc35daaf0bfad17fa7b67
IanClayman <Ian.clayman1@gmail.com>
2019-04-17 13:31:17 -0400

    Initial commit

---
+++ BetterQuickSlotsMod.cs
@@ -0,0 +1,225 @@
+﻿/* * * * *
+ * BetterQuickSlots
+ * --------------------
+ * "It Does What It Says"
+ * 
+ * Better Quick Slots is an improvement on the mod "Second Quick Slot Mod" (https://www.nexusmods.com/outward/mods/18) created by Emo#7953.
+ * It eliminates the stutter that occurred in that version when switching between the two quick slot bars and improves overall performance
+ * 
+ * Created by IAmTheClayman (Discord: IAmTheClayman#1280) with major credit to Emo#7953 for the original concept and tech support along the way.
+ * Would never have been able to make this without your help!
+ * 
+ * WHAT EXACTLY DOES IT DO
+ * Better Quick Slots allows the player to set two separate quick slot bars as opposed to Outward's default single bar.  Users can specify a custom keybinding
+ * to switch between the two skill bars in Mods/BetterQuickSlots_Config.json.
+ * 
+ * Please not that in current implementation the first time loading up the mod on a character will cause all previously-assigned  quick slots to be cleared.  Make sure
+ * to switch between the quick slot bars twice to fully clear skills before attempting to set new ones.  THIS MOD IS A PRE-1.0 RELEASE, so please be understanding as we make
+ * quality of life improvements and add features down the line.
+ * 
+ * PLANNED FEATURES
+ * - Right now the assigned keybinding will switch quick slot bars even while in inventory menus, during trading with merchants, during general NPC dialogue, etc.  Plans are to 
+ *      alter functionality to be more in line with general gameplay input (i.e. attacks getting locked out while in menus)
+ * - Ability to set the keybinding for switching quick slot bars in the default keyboard and controller keybinding menus, including the option to set up to 2 bindings
+ *      per control scheme as with every other input in Outward
+ * - UI elements to allow players to tell which of the two quick slot bars they are using at a glance, possibly with custom naming for each set
+ * 
+ * If you are reading this and have any questions feel free to contact me via Discord.  I am still learning my way around Outward modding (and modding in general) so I
+ * will do my best to assist you but please be patient and know that your programming skills may far exceed my own :p
+ * 
+ * * * * */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Partiality.Modloader;
+using SimpleJSON;
+
+namespace BetterQuickSlots
+{
+
+    public class BetterQuickSlotsMod : PartialityMod
+    {
+        // sbsPatch (SkillBarSwitcherPatch):Does the actual work related to loading into temp arrays, saving to/reading from JSON, etc.
+        public static ScriptLoad sbsPatch;
+
+        // modConfigFile: string declaring the location and name of the JSON file that stores each character's settings
+        public static string modConfigFile = "Mods/BetterQuickSlots_Config.json";
+
+        // modConfig: JSON object that allows us to actually work with the data saved in the modConfigFile
+        public SimpleJSON.JSONNode modConfig;
+
+        // currentCharacter: JSONNode that stores all data for the current character (name, keybind, skillbars, etc.)
+        public JSONNode currentCharacter;
+
+        // Constructor: sets the fields of the PartialityMod class
+        public BetterQuickSlotsMod()
+        {
+            this.ModID = "\"Better Quick Slots Mod\"";
+            this.Version = "0.1";
+            //this.loadPriority = 0;
+            this.author = "IAmTheClayman";
+        }
+
+        // ***Default/Required PartialityMod Methods***
+
+        // Init(): called when the mod is loaded
+        // Use to set properties or load configs
+        public override void Init()
+        {
+            base.Init();
+        }
+
+        // OnLoad(): called after ALL mods have loaded
+        // First-time load code goes here (creating objs, getting info from other mods, etc.)
+        public override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        // OnDisable(): called when a mod is disabled
+        public override void OnDisable()
+        {
+            base.OnDisable();
+        }
+
+        // OnEnable(): called when a mod is enabled
+        // Also called when a mod is loaded
+        public override void OnEnable()
+        {
+            // REQUIRED
+            base.OnEnable();
+
+            // ACTUAL WORK STARTS HERE
+            ScriptLoad.betterQuickSlots = this;
+
+            LoadConfig();
+
+            GameObject obj = new GameObject();
+            sbsPatch = obj.AddComponent<ScriptLoad>();
+            sbsPatch.Initialise();
+        }
+
+        // ***Helper functions for this specific mod***
+
+        public void LoadConfig()
+        {
+            // use System.IO to read the mod config file into a string
+            string json = File.ReadAllText(modConfigFile);
+
+            // convert the raw string into usable JSON object
+            modConfig = JSON.Parse(json);
+        }
+
+        public void SetCurrentCharacter(string charUID, string charName)
+        {
+            // 1st check if the modConfigFile has any entries.  If it does...
+            if (modConfig["characters"] != null)
+            {
+                // ...next check if the specific character requested has an entry, and if it does...
+                if (DoesCharacterExist(charUID))
+                {
+                    // Get the index of the desireed question in the array of characters, then
+                    // use that index to get the specific JSON object for that character
+                    int charSaveIndex = GetCharacterSaveIndex(charUID);
+                    var charSaveObject = modConfig["characters"][charSaveIndex];
+
+                    currentCharacter = charSaveObject;
+                }
+                // In the event that the desired character does not have an entry in the modConfigFile...
+                else
+                {
+                    currentCharacter = CreateNewCharacterSave(charUID, charName);
+                }
+            }
+
+            SaveConfig();
+
+        }
+
+        // Helper function used to check if a character has keybindings stored in the modConfigFile
+        public bool DoesCharacterExist(string charUID)
+        {
+            for (int i = 0; i < modConfig["characters"].AsArray.Count; i++)
+            {
+                string thisCharUID = modConfig["characters"][i]["characterUID"];
+
+                // if paramater charUID already exists, break out and return true
+                if (thisCharUID == charUID)
+                    return true;
+            }
+
+            // return false if no match is found
+            return false;
+        }
+
+        public int GetCharacterSaveIndex(string charUID)
+        {
+            for (int i = 0; i < modConfig["characters"].AsArray.Count; i++)
+            {
+                string thisCharUID = modConfig["characters"][i]["characterUID"];
+
+                if (thisCharUID == charUID)
+                    return i;
+            }
+
+            // Return 0 in event character cannot be found
+            return 0;
+        }
+
+        public JSONObject CreateNewCharacterSave(string charUID, string charName)
+        {
+            var newCharOBJ = new JSONObject();
+            modConfig["characters"][-1] = newCharOBJ;
+
+            // Start adding default values to new character's JSONObject
+            var jsonCharUID = new JSONString(charUID);
+            var jsonCharName = new JSONString(charName);
+            var jsonDefaultKeybind = new JSONString("tab");
+            var jsonFirstBarUIDs = new JSONArray();
+
+            jsonFirstBarUIDs[0] = 0;
+            jsonFirstBarUIDs[1] = 0;
+            jsonFirstBarUIDs[2] = 0;
+            jsonFirstBarUIDs[3] = 0;
+            jsonFirstBarUIDs[4] = 0;
+            jsonFirstBarUIDs[5] = 0;
+            jsonFirstBarUIDs[6] = 0;
+            jsonFirstBarUIDs[7] = 0;
+
+            var jsonSecondBarUIDs = new JSONArray();
+
+            jsonSecondBarUIDs[0] = 0;
+            jsonSecondBarUIDs[1] = 0;
+            jsonSecondBarUIDs[2] = 0;
+            jsonSecondBarUIDs[3] = 0;
+            jsonSecondBarUIDs[4] = 0;
+            jsonSecondBarUIDs[5] = 0;
+            jsonSecondBarUIDs[6] = 0;
+            jsonSecondBarUIDs[7] = 0;
+
+            // Now we begin assigning these values to the newly created JSONObject
+            newCharOBJ.Add("characterUID", jsonCharUID);
+            newCharOBJ.Add("characterName", jsonCharName);
+            newCharOBJ.Add("keybind", jsonDefaultKeybind);
+            newCharOBJ.Add("FirstBarUIDs", jsonFirstBarUIDs);
+            newCharOBJ.Add("SecondBarUIDs", jsonSecondBarUIDs);
+
+            return newCharOBJ;
+        }
+
+        public void SaveConfig()
+        {
+            File.WriteAllText(modConfigFile, modConfig.ToString());
+        }
+
+    }
+
+    [Serializable]
+    public class ModConfig
+    {
+        public string SwitchKeybinding;
+        public int[] firstBarUIDs;
+        public int[] secondBarUIDs;
+    }
+}
---
+++ ScriptLoad.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Reflection;
+using UnityEngine;
+using On;
+using Rewired;
+
+namespace BetterQuickSlots
+{
+    public class ScriptLoad : MonoBehaviour
+    {
+
+        // allows us to call and access methods in the betterQuickSlotsMod class (inherits from PartialityMod)
+        public static BetterQuickSlotsMod betterQuickSlots;
+
+        // Item arrays to use as temp storage for skills and items
+        // These are specicically item arrays b/c Outward treats skills as Items
+        // ( obviously items are also treated as Items :P )
+        public Item[] quickSlots1;
+        public Item[] quickSlots2;
+
+        // ***UPDATE DESCRIPTION ONCE I FIGURE OUT WHAT THESE DO***
+        //    (unused in current implementation of code)
+        public string[] defaultIDs = new string[8];
+        private bool isInited = false;
+
+        private int numSlots = 8;
+        private string charUID;
+
+        // When dev is set to true extra debugging messages will be printed
+        private bool dev = false;
+
+        // enum used to toggle btw the two skill bars
+        public enum BarMode
+        {
+            FIRST,
+            SECOND
+        }
+
+        // default to first bar on class init
+        public BarMode barMode = BarMode.FIRST;
+
+        // Used for "neatness" ( I don't know what that means but I trust Emo#7953's judgement here :) )
+        public void Initialise()
+        {
+            // Patch is the method that actually does all of the good stuff!
+            Patch();
+        }
+
+        // The Patch method uses hooks to add new functionality to existing methods across Outward
+        public void Patch()
+        {
+            On.PlayerSystem.Start += new On.PlayerSystem.hook_Start(PlayerSystem_StartHook);
+            On.Character.Update += new On.Character.hook_Update(Character_UpdateHook);
+
+            On.NetworkLevelLoader.LevelDoneLoading += new On.NetworkLevelLoader.hook_LevelDoneLoading(LevelDoneLoadingHook);
+            On.CharacterQuickSlotManager.OnAssigningQuickSlot_1 += new On.CharacterQuickSlotManager.hook_OnAssigningQuickSlot_1(CQSManager_OnAssigningQuickSlotHook);
+
+        }
+
+        // *** Section for Hooks ***
+
+        private void PlayerSystem_StartHook(On.PlayerSystem.orig_Start orig, PlayerSystem self)
+        {
+            // Run default PlayerSystem.Start() code first
+            orig(self);
+
+            // call LoadConfig() method from betterQuickSlotsMod class
+            betterQuickSlots.LoadConfig();
+
+            quickSlots1 = new Item[8];
+            quickSlots2 = new Item[8];
+
+            betterQuickSlots.SetCurrentCharacter(self.CharUID, self.Name);
+
+            //Set local class variable charUID to the appropriate UID from PlayerSystem
+            charUID = self.CharUID;
+
+            // The following line fixes a bug where items in quickSlots2[] would get loaded in on top of
+            // items loaded from quickSlots1[].  For this reason DO NOT REMOVE the following line without 
+            // rigorously testing the impact on mod perfomance!
+            PopulateSkillBar(self.ControlledCharacter.QuickSlotMngr);
+        }
+
+        private void Character_UpdateHook(On.Character.orig_Update orig, Character self)
+        {
+            orig(self);
+            var quickSlotManager = self.GetComponent<CharacterQuickSlotManager>();
+
+            if (quickSlotManager != null)
+            {
+                // First we check if the button assigned to keybind (aka the button to switch 
+                // btw skill bars) has actually been pressed
+                if (Input.GetKeyDown(betterQuickSlots.currentCharacter["keybind"]))
+                {
+                    // If the current BarMode is FIRST...
+                    if (barMode == BarMode.FIRST)
+                    {
+                        if (dev)
+                            Debug.Log("Switching to SECOND skill bar");
+
+                        // Set barMode to SECOND
+                        SetBarMode(BarMode.SECOND);
+                        // And repopulate the default-behaviour skill bar w/ entries from quickSlots2[]
+                        PopulateSkillBar(quickSlotManager);
+                    }
+                    // Else if the current BarMode is SECOND...
+                    else if (barMode == BarMode.SECOND)
+                    {
+                        if (dev)
+                            Debug.Log("Switching to FIRST skill bar");
+
+                        // Set barMode to FIRST
+                        SetBarMode(BarMode.FIRST);
+                        // And repopulate the default-behaviour skill bar w/ entries from quickSlots1[]
+                        PopulateSkillBar(quickSlotManager);
+                    }
+                }
+            }
+        }
+
+        private void LevelDoneLoadingHook(On.NetworkLevelLoader.orig_LevelDoneLoading orig, NetworkLevelLoader self)
+        {
+            orig(self);
+
+            //Debug.Log(System.DateTime.Now.ToShortTimeString() + " || Running LevelDoneLoadingHook");
+            //Character character = CharacterManager.Instance.GetCharacter(CharacterManager.Instance.PlayerCharacters.Values[0]);
+            //Debug.Log("Character Name: " + character.Name);
+
+            Character character = CharacterManager.Instance.GetCharacter(charUID);
+            var quickSlotManager = character.gameObject.GetComponent<CharacterQuickSlotManager>();
+            LoadBarEntriesFromJSON(quickSlotManager);
+            PopulateSkillBar(quickSlotManager);
+        }
+
+        private void CQSManager_OnAssigningQuickSlotHook(On.CharacterQuickSlotManager.orig_OnAssigningQuickSlot_1 orig, CharacterQuickSlotManager qsManager, Item _itemToQuickSlot)
+        {
+            orig(qsManager, _itemToQuickSlot);
+
+            if (dev)
+                Debug.Log("ASSINGING " + _itemToQuickSlot + " to slot index " + _itemToQuickSlot.QuickSlotIndex + " on " + barMode.ToString() + " skill bar");
+
+            if (_itemToQuickSlot.QuickSlotIndex == -1)
+            {
+                // Nothing to do here
+            }
+            else
+            {
+                SaveSkillSlotByIndex(_itemToQuickSlot.QuickSlotIndex, _itemToQuickSlot);
+            }
+        }
+
+        // *** Section for Helper Functions ***
+
+        // LoadBarEntriesFromJSON populates quickSlots1[] and quickSlots2[] using the data from
+        // the modConfig file saved in betterQuickSlots
+        private void LoadBarEntriesFromJSON(CharacterQuickSlotManager qsManager)
+        {
+            CharacterInventory inventory = qsManager.gameObject.GetComponent<CharacterInventory>();
+            CharacterSkillKnowledge knownSkills = inventory.SkillKnowledge;
+            ItemManager itemManager = ItemManager.Instance;
+
+            // Load items and skills into quickSlots1[]
+            for (int i = 0; i < numSlots; i++)
+            {
+                if (dev)
+                    Debug.Log("LOADING SKILLBAR SLOT " + i + "FOR 1ST BAR");
+
+                var entryUID = betterQuickSlots.currentCharacter["FirstBarUIDs"][i];
+
+                // If there is actually a skill/item stored at the index being looked at in FirstBarUIDs[]...
+                if (entryUID != 0)
+                {
+                    if (dev)
+                        Debug.Log("UID of skill/item is : " + entryUID);
+
+                    // This line loads a skill into the variable called "entry"...
+                    var entry = knownSkills.GetItemFromItemID(entryUID.AsInt);
+
+                    // ...But if "entry" isn't a skill, load an item instead
+                    if (entry == null)
+                        entry = inventory.GetOwnedItems(entryUID).First();
+
+                    quickSlots1[i] = entry;
+                }
+                // If there isn't a skill/item stored at the index being looked at in FirstBarUIDs[]...
+                else
+                {
+                    if (dev)
+                        Debug.Log("Skill bar slot " + i + " has no assigned skill/item");
+                }
+            }
+
+            // Load items and skills into quickSlots2[]
+            for (int i = 0; i < numSlots; i++)
+            {
+                if (dev)
+                    Debug.Log("LOADING SKILLBAR SLOT " + i + "FOR 2ND BAR");
+
+                var entryUID = betterQuickSlots.currentCharacter["SecondBarUIDs"][i];
+
+                // If there is actually a skill/item stored at the index being looked at in SecondBarUIDs[]...
+                if (entryUID != 0)
+                {
+                    if (dev)
+                        Debug.Log("UID of skill/item is : " + entryUID);
+
+                    // This line loads a skill into the variable called "entry"...
+                    var entry = knownSkills.GetItemFromItemID(entryUID.AsInt);
+
+                    // ...But if "entry" isn't a skill, load an item instead
+                    if (entry == null)
+                        entry = inventory.GetOwnedItems(entryUID).First();
+
+                    quickSlots2[i] = entry;
+                }
+                // If there isn't a skill/item stored at the index being looked at in SecondBarUIDs[]...
+                else
+                {
+                    if (dev)
+                        Debug.Log("Skill bar slot " + i + " has no assigned skill/item");
+                }
+            }
+        }
+
+        // PopulateSkillBar populates the default-behaviour Outward skill bar with enties from either
+        // quickSlots1[] or quickSlots2[] based on the value of barMode
+        private void PopulateSkillBar(CharacterQuickSlotManager qsManager)
+        {
+            if (qsManager.isActiveAndEnabled)
+            {
+                if (dev)
+                    Debug.Log("Populating Skill Bar");
+
+                // The entries we are going to use to populate Outward's default-behaviour skill bar is dependent on
+                // whether we want the first skill bar or the second skill bar
+                switch (barMode)
+                {
+                    // In the case where we want entries from the first skill bar...
+                    case BarMode.FIRST:
+                        for (int i = 0; i < numSlots; i++)
+                        {
+                            // If the entry we want to add to the skill bar is actually exists...
+                            if (quickSlots1[i] != null)
+                            {
+                                if (dev)
+                                    Debug.Log("Populating skillbar slot " + i + " with entry " + quickSlots1[i]);
+                                // Actually assign the entry to the corresponding slot
+                                qsManager.SetQuickSlot(i, quickSlots1[i], false);
+                            }
+                            // If the entry to add to the skill bar DOESN'T exist...
+                            else
+                            {
+                                if (dev)
+                                    Debug.Log("No assignable entry.  Clearing slot");
+                                // Clear the slot
+                                qsManager.ClearQuickSlot(i);
+                            }
+                        }
+                        // We are done doing stuff in the case of the first skill bar, so BREAK!
+                        break;
+                    // In the case where we want to add entries from the second skill bar...
+                    case BarMode.SECOND:
+                        for (int i = 0; i < numSlots; i++)
+                        {
+                            // If the entry we want to add to the skill bar is actually exists...
+                            if (quickSlots2[i] != null)
+                            {
+                                if (dev)
+                                    Debug.Log("Populating skillbar slot " + i + " with entry " + quickSlots2[i]);
+                                // Actually assign the entry to the corresponding slot
+                                qsManager.SetQuickSlot(i, quickSlots2[i], false);
+                            }
+                            // If the entry to add to the skill bar DOESN'T exist...
+                            else
+                            {
+                                if (dev)
+                                    Debug.Log("No assignable entry.  Clearing slot");
+                                // Clear the slot
+                                qsManager.ClearQuickSlot(i);
+                            }
+                        }
+                        // We are done doing stuff in the case of the second skill bar, so BREAK!
+                        break;
+                }
+
+            }
+
+        }
+
+        private void SaveSkillSlotByIndex(int index, Item item)
+        {
+            // What we do here depends on which skill bar is currently active
+            switch (barMode)
+            {
+                case BarMode.FIRST:
+                    if (dev)
+                        Debug.Log("Setting quickSlots1[" + index + "] to " + item);
+
+                    quickSlots1[index] = item;
+                    betterQuickSlots.currentCharacter["FirstBarUIDs"][index] = item.ItemID;
+
+                    break;
+
+                case BarMode.SECOND:
+                    if (dev)
+                        Debug.Log("Setting quickSlots2[" + index + "] to " + item);
+
+                    quickSlots2[index] = item;
+                    betterQuickSlots.currentCharacter["SecondBarUIDs"][index] = item.ItemID;
+
+                    break;
+            }
+
+            betterQuickSlots.SaveConfig();
+        }
+
+        private void SetBarMode(BarMode bm)
+        {
+            barMode = bm;
+        }
+    }
+}
